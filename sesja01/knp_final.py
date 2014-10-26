6cffc63debd56d575b6ae5f7963a2a30c90d1930
soutys <soutys@tlen.pl>
2014-10-26 20:12:05 +0100

    komentarze do kodu + poprawki literówek i wcięć

--- sesja01/knp_final.py
+++ sesja01/knp_final.py
@@ -1,28 +1,45 @@
+'''Gra w 
+'''
+
 import random
 
+# flaga kontynuacji gry: True = gramy (kolejna) runde
 keep_playing = True
 
+# glowna petla gry
 while keep_playing:
+    # gracz podaje swoj symbol
     player_choice = input("[k]amień, [n]ożyczki, [p]apier?")
+    # komputer losuje wlasny symbol
     computer_choice = random.choice("knp")
+    # prezentacja dokonanych wyborow symboli
     print("Ty wybrałeś", player_choice)
     print("Ja wybrałem", computer_choice)
 
+    # flaga wygranej komputera
     player_victory = False
 
-    if player_choice == "k" and computer_choice = "n":
-    	player_victory = True
-    elif player_choice == "n" and computer_choice = "p":
-    	player_victory = True
-    elif player_choice == "p" and computer_choice = "k":
-    	player_victory = True
+    # porownujemy symbole
+    if player_choice == "k" and computer_choice == "n":
+        # kamien (gracz) vs nozyczki (komputer) = wygrywa kamien (czyli gracz)
+        player_victory = True
+    elif player_choice == "n" and computer_choice == "p":
+        # nozyczki (gracz) vs papier (komputer) = wygrywaja nozyczki
+        player_victory = True
+    elif player_choice == "p" and computer_choice == "k":
+        # papier (gracz) vs kamien (komputer) = wygrywa papier
+        player_victory = True
 
+    # prezentacja zwyciezcow
     if player_victory:
-    	print("Wygrałeś!")
+        print("Wygrałeś!")
     else:
-    	print("Wygrałem, yay! :P")
+        print("Wygrałem, yay! :P")
 
+    # zapytanie o kontynuacje gry
     finish_game = input("Koniec gry (t/n)?")
 
+    # czy konczymy?
     if finish_game == "t":
+        # tak, konczymy...
         keep_playing = False
