e69d153624ad6f6d26c220d6e7fbcf0ce997b6be
Przemysław Konopko <przemyslaw.konopko@grupawp.pl>
2014-11-01 14:45:19 +0100

    uzmiennienie testów, głównie porządki i poprawki drobnych błędów (scope, test odpowiedzi, wyświetlanie odpowiedzi, PL-literki w komunikatach)

--- sesja02/framework_mastermind.py
+++ sesja02/framework_mastermind.py
@@ -1,90 +1,110 @@
+# -*- coding: utf-8 -*-
+
 '''
-Gra w mastermind, ale w powiedzmy "frameworku"
+Klon gry Mastermind oparty na "frameworku"
 '''
 
-# Funkcje frameworkowe z implementacja masterminda
+# Funkcje frameworkowe z implementacja zasad gry Mastermind
 
-def gameDescription():
-    print ("MasterMind: gra polega na odgadnieciu trzyliterowego kodu skladajacego sie z liter R, G, B lub Y.")
-    print ("Masz na to 10 prob.")
+def getMaxNumberOfGuesses():
+    return 10
 
-def createRiddle():
+
+def getGuessCount():
+    return 3
+
+
+def getRiddleSet():
+    return "RBGY"
+
+
+def printGameDescription():
+    print("Mastermind: gra polega na odgadnięciu %s-literowego kodu składającego się z liter %s." % (
+        getGuessCount(),
+        getRiddleSet(),
+    ))
+    print("Masz na to %s prób(y)." % getMaxNumberOfGuesses())
+
+
+def getRiddle():
     riddle = ""
-    for i in list(range(3)):
-        riddle = riddle + random.choice("RBGY")
-    print ("Riddle: ", riddle)
+    for i in range(getGuessCount()):
+        riddle += random.choice(getRiddleSet())
+#    riddle = ''.join(random.choice(getRiddleSet())
+#        for _ in range(getGuessCount()))
+#    print ("Riddle: ", riddle)
     return riddle
 
-def userGuessPrompt():
+
+def getUserGuess():
     return input("Podaj kod: ")
 
+
 def evaluateGuess(riddle, player_choice):
-    if riddle_guessed == player_choice:
+    if riddle == player_choice:
         return True
     else:
         correct_place = 0
         correct_not_in_place = 0
-        for i in list(range(3)):
+        for i in range(getGuessCount()):
             if riddle[i] == player_choice[i]:
-                correct_place = correct_place + 1
+                correct_place += 1
             elif player_choice[i] in riddle:
-                    correct_not_in_place = correct_not_in_place + 1
-        print ("Prawidlowe miejsce: ", correct_place)
+                    correct_not_in_place += 1
+        print ("Prawidłowe miejsce: ", correct_place)
         print ("Kod jest ale w innym miejscu: ", correct_not_in_place)
 
 
-def congratulate(riddle) :
-    print("Gratulacje! Zgadles kod. Kod to ", riddle)
+def printCongratulations(riddle):
+    print("Gratulacje! Zgadłeś kod. Kod to:", riddle)
 
-
-def getMaxNumberOfGuesses() :
-    return 10
 
 def validateGuess(user_guess):
     correct_chars = True
     for letter in user_guess:
-        if letter not in "RBGY":
+        if letter not in getRiddleSet():
             correct_chars = False
-    if len(user_guess) == 3 and correct_chars:
+    if len(user_guess) == getGuessCount() and correct_chars:
         return True
     else:
-        print ("Neprawidlowa odpowiedz.")
+        print ("Nieprawidłowa odpowiedź.")
         return False
 
 # gra wlasciwa
 
 import random
 
-gameDescription()
+printGameDescription()
 # flaga kontynuacji gry: True = gramy (kolejna) runde
 keep_playing = True
 max_guesses = getMaxNumberOfGuesses()
 
 # glowna petla gry
 while keep_playing:
-    #glowna petla
-    riddle = createRiddle()
+    riddle = getRiddle()
     riddle_guessed = False
     guess = 0
     while guess < max_guesses and not riddle_guessed:
         print ("Proba: ", guess + 1)
         user_guess_valid = False
         while not user_guess_valid:
-            user_guess = userGuessPrompt()
+            user_guess = getUserGuess()
             user_guess_valid = validateGuess(user_guess)
         riddle_guessed = evaluateGuess(riddle, user_guess)
-        guess = guess + 1
+        guess += 1
 
-    #sprawdzamy czy gra sie skonczyla, bo wyczerpala sie ilosc prob
-    #czy dlatego, ze gracz zgadl
+    # sprawdzamy czy gra sie skonczyla, bo wyczerpala sie ilosc prob
+    # czy dlatego, ze gracz zgadl
     if riddle_guessed:
-        congratulate()
+        printCongratulations(riddle)
     else:
-        print("Nie ma wiecej prob. Przegrales :(")
+        print("Nie ma więcej prób. Przegrałeś :(")
+
     # zapytanie o kontynuacje gry
     next_game = input("Jeszcze jedna zagadka (T/n)?")
-
     if next_game == "n":
-        # tak, konczymy...
+        # nie, konczymy...
         keep_playing = False
 
+
+# vim: ts=4:sw=4:et:fdm=indent:ff=unix
